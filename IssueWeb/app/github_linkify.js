9ec6f3a9da2fd431d3fd8b274ca1f07e99edbd25
James Howard <jameshoward@mac.com>
2016-04-26 15:20:34 -0700

    Fix #60

--- IssueWeb/app/github_linkify.js
+++ IssueWeb/app/github_linkify.js
@@ -1,6 +1,7 @@
 export function githubLinkify(owner, repo, text) {
   var issues = /(^|[^\&<>])([\w+\-\d]+)?\/?([\w+\-\d]+)?#(\d+)/g;
-  var mentions = /(^|[^<>])@([\w+\-\d]+)/g;
+  var mentions = /(^| )@([\w+\-\d]+)/g;
+  var hashes = /([\w+\-\d]+\/)?([\w+\-\d]+@)?([A-Fa-f0-9]{40})/g;
 
   try {
     return text.replace(issues, function(g0, g1, g2, g3, g4) {
@@ -13,6 +14,14 @@ export function githubLinkify(owner, repo, text) {
       }
     }).replace(mentions, function(g0, g1, g2) {
       return g1 + '<a class="mentionLink" href="https://github.com/' + g2 + '">' + g0 + "</a>";
+    }).replace(hashes, function(g0, g1, g2, g3) {
+      if (g1 && g2) {
+        return '<a class="shaLink" href="https://github.com/' + g1.slice(0, -1) + '/' + g2.slice(0, -1) + '/commit/' + g3 + '">' + g1 + g2 + g3.slice(0, 7) + "</a>";
+      } else if (g2) {
+        return '<a class="shaLink" href="https://github.com/' + owner + '/' + repo + '/commit/' + g3 + '">' + g2 + g3.slice(0, 7) + "</a>";
+      } else {
+        return '<a class="shaLink" href="https://github.com/' + owner + '/' + repo + '/commit/' + g3 + '">' + g3.slice(0, 7) + "</a>";
+      }
     });
   } catch (ex) {
     console.log(ex);
