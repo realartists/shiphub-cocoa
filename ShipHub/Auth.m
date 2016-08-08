e950bfc21bc1bd19c03803f1a70e6dfcfc3c48fe
James Howard <jameshoward@mac.com>
2016-08-08 11:46:12 -0700

    Remove Authorisation

--- ShipHub/Auth.m
+++ ShipHub/Auth.m
@@ -206,13 +206,7 @@ NSString *const AuthStatePreviousKey = @"AuthStatePrevious";
 }
 
 - (void)addAuthHeadersToRequest:(NSMutableURLRequest *)request {
-    [request setValue:nil forHTTPHeaderField:@"Authorization"];
-    if ([[[request URL] host] hasSuffix:@"realartists.com"]) {
-        // Authorisation with an 's' to work around IIS HTTP/2 bug
-        [request setValue:[NSString stringWithFormat:@"token %@", self.token] forHTTPHeaderField:@"Authorisation"];
-    } else {
-        [request setValue:[NSString stringWithFormat:@"token %@", self.ghToken] forHTTPHeaderField:@"Authorization"];
-    }
+    [request setValue:[NSString stringWithFormat:@"token %@", self.ghToken] forHTTPHeaderField:@"Authorization"];
 }
 
 - (void)logout {
