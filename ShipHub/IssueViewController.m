5dc030db46ac829154bc265caff464b1c64ff75c
James Howard <jameshoward@mac.com>
2016-08-14 23:32:04 -0700

    Fix realartists/shiphub-cocoa#212 IssueViewController should scroll to top when changing issue

--- ShipHub/IssueViewController.m
+++ ShipHub/IssueViewController.m
@@ -177,12 +177,16 @@ static NSString *const WebpackDevServerURL = @"http://localhost:8080/";
 - (void)setIssue:(Issue *)issue {
     dispatch_assert_current_queue(dispatch_get_main_queue());
     //DebugLog(@"%@", issue);
+    BOOL issueChanged = issue != nil && _issue != nil && ![_issue.fullIdentifier isEqualToString:issue.fullIdentifier];
     _issue = issue;
     if (issue) {
         NSString *issueJSON = [self issueStateJSON:issue];
         NSString *js = [NSString stringWithFormat:@"applyIssueState(%@)", issueJSON];
         //DebugLog(@"%@", js);
         [self evaluateJavaScript:js];
+        if (issueChanged) {
+            [self evaluateJavaScript:@"window.scroll(0, 0)"];
+        }
     }
     [self updateTitle];
     _web.hidden = _issue == nil;
