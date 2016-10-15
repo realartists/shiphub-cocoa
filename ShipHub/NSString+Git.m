7c1f2d2458953f7cf6d11ee0be2e6834f9adc7ac
James Howard <jameshoward@mac.com>
2016-10-14 22:03:12 -0700

    don't try to infer text file encoding. just use UTF8 and hope for the best

--- ShipHub/NSString+Git.m
+++ ShipHub/NSString+Git.m
@@ -10,38 +10,17 @@
 
 @implementation NSString (Git)
 
-static NSDictionary *encodingOpts() {
-    static dispatch_once_t onceToken;
-    static NSDictionary *stringOpts;
-    dispatch_once(&onceToken, ^{
-        stringOpts = @{
-            NSStringEncodingDetectionSuggestedEncodingsKey : @[@(NSUTF8StringEncoding)],
-            NSStringEncodingDetectionAllowLossyKey : @NO,
-            NSStringEncodingDetectionLikelyLanguageKey : @"en"
-        };
-    });
-    return stringOpts;
-}
-
 + (NSString *)stringWithGitBlob:(const git_blob *)blob {
     const void *rawContents = git_blob_rawcontent(blob);
     size_t rawContentsLength = git_blob_rawsize(blob);
-    
-    NSData *data = [NSData dataWithBytes:rawContents length:rawContentsLength];
-    NSString *result = nil;
-    [NSString stringEncodingForData:data encodingOptions:encodingOpts() convertedString:&result usedLossyConversion:NULL];
-    
-    return result;
+    return [[NSString alloc] initWithBytes:rawContents length:rawContentsLength encoding:NSUTF8StringEncoding];
 }
 
 + (NSString *)stringWithGitBuf:(const git_buf *)buf {
     if (!buf->ptr) return nil;
     if (buf->size == 0) return @"";
     
-    NSData *data = [NSData dataWithBytes:buf->ptr length:buf->size];
-    NSString *result = nil;
-    [NSString stringEncodingForData:data encodingOptions:encodingOpts() convertedString:&result usedLossyConversion:NULL];
-    return result;
+    return [[NSString alloc] initWithBytes:buf->ptr length:buf->size encoding:NSUTF8StringEncoding];
 }
 
 @end
