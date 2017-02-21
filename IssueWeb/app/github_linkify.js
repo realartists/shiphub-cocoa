cb5feeb6adaed3f5e6759071996e5ba1cd145a6c
James Howard <jameshoward@mac.com>
2017-02-21 10:01:02 -0800

    Fix realartists/shiphub-cocoa#409 (#49) in markdown renders as ((#49)

--- IssueWeb/app/github_linkify.js
+++ IssueWeb/app/github_linkify.js
@@ -6,14 +6,14 @@ export function githubLinkify(owner, repo, text) {
   try {
     return text.replace(issues, function(g0, g1, g2, g3, g4) {
       if (g2 == undefined && g3 == undefined) {
-        return g1 + '<a class="issueLink" href="https://github.com/' + owner + '/' + repo + '/issues/' + g4 + '">' + g0 + "</a>";
-      } else if (g2 == undefined) {
-        return g1 + '<a class="issueLink" href="https://github.com/' + owner + '/' + g3 + '/issues/' + g4 + '">' + g0 + "</a>";
+        return g1 + '<a class="issueLink" href="https://github.com/' + owner + '/' + repo + '/issues/' + g4 + '">' + g0.substr(g1.length) + "</a>";
+      } else if (g3 == undefined) {
+        return g1 + '<a class="issueLink" href="https://github.com/' + owner + '/' + g2 + '/issues/' + g4 + '">' + g0.substr(g1.length) + "</a>";
       } else {
-        return g1 + '<a class="issueLink" href="https://github.com/' + g2 + '/' + g3 + '/issues/' + g4 + '">' + g0 + "</a>";
+        return g1 + '<a class="issueLink" href="https://github.com/' + g2 + '/' + g3 + '/issues/' + g4 + '">' + g0.substr(g1.length) + "</a>";
       }
     }).replace(mentions, function(g0, g1, g2) {
-      return g1 + '<a class="mentionLink" href="https://github.com/' + g2 + '">' + g0 + "</a>";
+      return g1 + '<a class="mentionLink" href="https://github.com/' + g2 + '">' + g0.substr(g1.length) + "</a>";
     }).replace(hashes, function(g0, g1, g2, g3) {
       if (g1 && g2) {
         return '<a class="shaLink" href="https://github.com/' + g1.slice(0, -1) + '/' + g2.slice(0, -1) + '/commit/' + g3 + '">' + g1 + g2 + g3.slice(0, 7) + "</a>";
