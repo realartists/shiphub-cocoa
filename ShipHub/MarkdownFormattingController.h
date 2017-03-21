55409c7f334db1d56fe61ae79b6e1bfaff6efa37
James Howard <jameshoward@mac.com>
2017-03-21 14:19:21 -0700

    Fix realartists/shiphub-cocoa#420 Hook up format menu for diff comments Fix realartists/shiphub-cocoa#421 Hook up touch bar for diff comments

---
+++ ShipHub/MarkdownFormattingController.h
@@ -0,0 +1,13 @@
+//
+//  MarkdownFormattingController.h
+//  ShipHub
+//
+//  Created by James Howard on 3/21/17.
+//  Copyright © 2017 Real Artists, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MarkdownFormattingController : NSObject
+
+@end
---
+++ ShipHub/MarkdownFormattingController.m
@@ -0,0 +1,13 @@
+//
+//  MarkdownFormattingController.m
+//  ShipHub
+//
+//  Created by James Howard on 3/21/17.
+//  Copyright © 2017 Real Artists, Inc. All rights reserved.
+//
+
+#import "MarkdownFormattingController.h"
+
+@implementation MarkdownFormattingController
+
+@end
