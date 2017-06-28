df7bf1e5fce21fcbbce30ddf2bf117727398156d
James Howard <jameshoward@mac.com>
2017-06-28 15:18:41 -0700

    Fix realartists/shiphub-cocoa#582 Has anyone ever printed? (log printing to mixpanel)

---
+++ ShipHub/Application.h
@@ -0,0 +1,14 @@
+//
+//  Application.h
+//  ShipHub
+//
+//  Created by James Howard on 6/28/17.
+//  Copyright © 2017 Real Artists, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <HockeySDK/HockeySDK.h>
+
+@interface Application : BITCrashExceptionApplication
+
+@end
---
+++ ShipHub/Application.m
@@ -0,0 +1,22 @@
+//
+//  Application.m
+//  ShipHub
+//
+//  Created by James Howard on 6/28/17.
+//  Copyright © 2017 Real Artists, Inc. All rights reserved.
+//
+
+#import "Application.h"
+
+#import "Analytics.h"
+
+@implementation Application
+
+- (BOOL)sendAction:(SEL)action to:(id)target from:(id)sender {
+    if (action == @selector(print:)) {
+        [[Analytics sharedInstance] track:@"Print"];
+    }
+    return [super sendAction:action to:target from:sender];
+}
+
+@end
