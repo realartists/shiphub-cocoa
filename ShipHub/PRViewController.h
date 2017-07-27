5beb0a44033cb036e0ce0a5f00269dae2309ee27
James Howard <jameshoward@mac.com>
2017-07-27 14:50:13 -0700

    Fix realartists/shiphub-cocoa#609 Formalize PRNavigationInformalProtocol

--- ShipHub/PRViewController.h
+++ ShipHub/PRViewController.h
@@ -10,8 +10,9 @@
 
 #import "PullRequest.h"
 #import "Issue.h"
+#import "PRNavigationActionResponder.h"
 
-@interface PRViewController : NSViewController
+@interface PRViewController : NSViewController <PRNavigationActionResponder>
 
 - (void)loadForIssue:(Issue *)issue;
 
@@ -22,18 +23,6 @@
 @property (readonly) NSToolbar *toolbar; // toolbar for the window we're in
 
 @property (readonly, getter=isInReview) BOOL inReview;
-
-- (IBAction)nextFile:(id)sender;
-- (IBAction)previousFile:(id)sender;
-
-- (IBAction)nextThing:(id)sender;
-- (IBAction)previousThing:(id)sender;
-
-- (IBAction)nextChange:(id)sender;
-- (IBAction)previousChange:(id)sender;
-
-- (IBAction)nextComment:(id)sender;
-- (IBAction)previousComment:(id)sender;
 
 - (IBAction)merge:(id)sender;
 
