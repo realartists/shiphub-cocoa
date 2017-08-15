cd3a828afcbd7e0f1e7c299b79c7ecf45deb772a
James Howard <jameshoward@mac.com>
2017-08-15 16:52:02 -0700

    Customize commit message for squash, disable commit message for rebase

--- ShipHub/PRMergeViewController.m
+++ ShipHub/PRMergeViewController.m
@@ -11,13 +11,16 @@
 #import "Extras.h"
 #import "Repo.h"
 
-@interface PRMergeViewController ()
+@interface PRMergeViewController () <NSTextFieldDelegate, NSTextViewDelegate>
 
 @property IBOutlet NSTextField *titleField;
 @property IBOutlet NSTextView *messageView;
 @property IBOutlet NSButton *mergeButton;
 @property IBOutlet NSButton *squashButton;
 @property IBOutlet NSButton *rebaseButton;
+
+@property BOOL titleEdited;
+@property BOOL messageEdited;
 
 @end
 
@@ -26,6 +29,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     _mergeButton.state = NSOnState;
+    [_titleField setDelegate:self];
+    [_messageView setDelegate:self];
 }
 
 - (void)setPr:(PullRequest *)pr {
@@ -37,12 +42,54 @@
     }
 }
 
+- (void)updateTitleAndMessage {
+    Issue *i = _issue;
+    
+    _titleField.enabled = YES;
+    _messageView.enabled = YES;
+    
+    if (_mergeButton.state == NSOnState) {
+        if (!_titleEdited) {
+            _titleField.stringValue = i != nil ? [NSString stringWithFormat:@"Merge pull request #%@ from %@/%@", i.number, i.repository.fullName, i.head[@"ref"]] : @"";
+        }
+        if (!_messageEdited) {
+            _messageView.string = i.title ?: @"";
+        }
+    } else if (_squashButton.state == NSOnState) {
+        if (!_titleEdited) {
+            _titleField.stringValue = i != nil ? [NSString stringWithFormat:@"%@ (#%@)", i.title, i.number] : @"";
+        }
+        if (!_messageEdited) {
+            _messageView.string = @"";
+        }
+    } else /* if (_rebaseButton.state == NSOnState) */ {
+        if (!_titleEdited) {
+            _titleField.placeholderString = NSLocalizedString(@"Not Applicable", nil);
+            _titleField.stringValue = @"";
+        }
+        _titleField.enabled = NO;
+        _messageView.enabled = NO;
+    }
+}
+
+- (void)controlTextDidChange:(NSNotification *)notification {
+    if (notification.object == _titleField) {
+        _titleEdited = YES;
+    }
+}
+
+- (void)textDidChange:(NSNotification *)notification {
+    if (notification.object == _messageView) {
+        _messageEdited = YES;
+    }
+}
+
 - (void)setIssue:(Issue *)i {
     [self view];
     if (_issue != i) {
         _issue = i;
-        _titleField.stringValue = i != nil ? [NSString stringWithFormat:@"Merge pull request #%@ from %@/%@", i.number, i.repository.fullName, i.head[@"ref"]] : @"";
-        _messageView.string = i.title ?: @"";
+        _titleEdited = _messageEdited = NO;
+        [self updateTitleAndMessage];
     }
 }
 
@@ -50,6 +97,7 @@
     for (NSButton *b in @[_mergeButton, _squashButton, _rebaseButton]) {
         b.state = sender == b ? NSOnState : NSOffState;
     }
+    [self updateTitleAndMessage];
 }
 
 - (IBAction)help:(id)sender {
