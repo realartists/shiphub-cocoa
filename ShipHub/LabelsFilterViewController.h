6f91a5f65bd938129b06a61000b7f2192ff1733b
James Howard <jameshoward@mac.com>
2017-10-13 08:16:32 -0700

    WIP: More label filtering

---
+++ ShipHub/LabelsFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  LabelsFilterViewController.h
+//  Ship
+//
+//  Created by James Howard on 10/9/17.
+//  Copyright © 2017 Real Artists, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LabelsFilterViewController : NSViewController
+
+@end
---
+++ ShipHub/LabelsFilterViewController.m
@@ -0,0 +1,22 @@
+//
+//  LabelsFilterViewController.m
+//  Ship
+//
+//  Created by James Howard on 10/9/17.
+//  Copyright © 2017 Real Artists, Inc. All rights reserved.
+//
+
+#import "LabelsFilterViewController.h"
+
+@interface LabelsFilterViewController ()
+
+@end
+
+@implementation LabelsFilterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
